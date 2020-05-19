8e80d3ee0000abb64a049733362c79603b33e647
thibault mambour <thibault.mambour@protonmail.com>
2020-05-19 15:25:43 +0200

    bartender controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,27 +2,6 @@ class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  def index
-  	@users = User.all
-  end
-
-  def show
-  end
-
-  def new
-  	@user = User.new
-  end
-
-  def create
-  	@user = User.new(user_params)
-
-  	if @user.save
-  	  redirect_to user_path(@user)
-  	else 
-  		render :new
-    end
-  end
-
   def edit
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :bartenders do
     resources :bookings, only: [:new, :create, :show] 
+    resources :reviews
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
