df501cf37527358302fbcbf97246457fe9ccfa0e
thibault mambour <thibault.mambour@protonmail.com>
2020-05-19 21:56:53 +0200

    Update the booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -15,13 +15,32 @@ class BookingsController < ApplicationController
   end
 
   def create
-  	@booking = @bartender.bookings.new(booking_params)
-  	@booking.user = current_user
-  	if @booking.save
-  	  redirect_to user_bookings_path(current_user)
-  	else 
-  		render :new
+    @bookings_bartender = Booking.where(:bartender_id => params[:bartender_id])
+    start_date = params[:booking][:start_date]
+    start_date = Date.parse(start_date)
+    end_date = params[:booking][:end_date]
+    end_date = Date.parse(end_date)
+    @bookings_bartender.each do |booking|
+      b_start_date = booking.start_date
+      b_end_date = booking.end_date
+      range = b_start_date..b_end_date
+      if (range === start_date) || (range === end_date)
+        @booking = Booking.new
+        @incorrect = "yes"
+        render :new
+        return
+      end
     end
+
+    @booking = @bartender.bookings.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to user_bookings_path(current_user)
+    else 
+      render :new
+    end
+
+
   end
 
   def destroy
