181b6b40d421eba4f627d786ecf9639c50fcc15a
Carl Boden <carl_boden@hotmail.com>
2020-05-19 15:32:56 +0200

    Reviews part1

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   resources :bartenders do
-    resources :bookings, only: [:new, :create, :show] 
+    resources :bookings, only: [:new, :create, :show]
+    resources :reviews, only: [:index, :show, :new, :create]
   end
+    resources :reviews, only: [ :destroy ]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
