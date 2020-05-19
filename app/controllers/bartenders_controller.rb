31b53cd8c11095ac6f85227c3424d74c49987728
thibault mambour <thibault.mambour@protonmail.com>
2020-05-19 20:36:28 +0200

    Update the review - ratings

--- app/controllers/bartenders_controller.rb
+++ app/controllers/bartenders_controller.rb
@@ -9,6 +9,10 @@ class BartendersController < ApplicationController
     @bartender = Bartender.find(params[:id])
     @user = User.find(@bartender.user_id)
     @reviews = @bartender.reviews
+    sum_rating = 0.00
+    @reviews.each{ |review| sum_rating += review.rating }
+    @reviews.length == 0 ? @average_rating = 0.00 : @average_rating = sum_rating / @reviews.length
+    
   end
 
   private
