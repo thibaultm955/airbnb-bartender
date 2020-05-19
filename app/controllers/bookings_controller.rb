f323fae60cd8781dceaabd166bb4e174f2e3afbd
vanessa massiot <vanessa.massiot@free.fr>
2020-05-19 10:56:35 +0200

    reviews bookings users controllers bases - still have to do the associations to references

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,47 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
+  def index
+  	@bookings = Booking.all
+  end
+
+  def show
+  end
+
+  def new
+  	@booking = Booking.new
+  end
+
+  def create
+  	@booking = Booking.new(booking_params)
+
+  	if @booking.save
+  	  redirect_to booking_path(@booking)
+  	else 
+  		render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  	@booking.update(booking_params)
+  	redirect_to booking_path(@booking)
+  end
+
+  def destroy
+  	@booking.destroy
+  	redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+  	params.require(:booking).permit(:start_date, :end_date)
+  end
+
+  def set_booking
+  	@booking = Booking.find(params[:id])
+  end
 end
