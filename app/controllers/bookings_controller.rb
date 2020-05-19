0fe0ce972fc663d971ed53835a6b7e0e584097e3
vanessa massiot <vanessa.massiot@free.fr>
2020-05-19 13:07:50 +0200

    bookings controller and bartenders controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,24 +1,21 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+  before_action :find_bartender, only: [:new, :create]
 
   def index
   	@bookings = Booking.all
   end
 
   def show
+  	@booking = Booking.find(params[:id])
   end
 
   def new
-  	@bartender = Bartender.find(params[:bartender_id])
   	@booking = Booking.new
   end
 
   def create
-  	@booking = Booking.new(booking_params)
-  	@user = User.find(params[:user_id])
-  	@bartender = Bartender.find(params[:bartender_id])
-  	@booking.bartender = @bartender
-  	@booking.user = @user
+  	@booking = @bartender.bookings.new(booking_params)
+  	@booking.user = current_user
 
   	if @booking.save
   	  redirect_to booking_path(@booking)
@@ -27,18 +24,6 @@ class BookingsController < ApplicationController
     end
   end
 
-  def edit
-  end
-
-  def update
-  	@booking.update(booking_params)
-  	redirect_to booking_path(@booking)
-  end
-
-  def destroy
-  	@booking.destroy
-  	redirect_to bookings_path
-  end
 
   private
 
@@ -46,7 +31,8 @@ class BookingsController < ApplicationController
   	params.require(:booking).permit(:start_date, :end_date)
   end
 
-  def set_booking
-  	@booking = Booking.find(params[:id])
+  def find_bartender
+  	@bartender = Bartender.find(params[:bartender_id])
   end
+
 end
